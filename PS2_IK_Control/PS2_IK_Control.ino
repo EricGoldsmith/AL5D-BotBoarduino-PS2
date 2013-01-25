cccff51fb2ad8d202ecd3abaebc6b59041873d60
Eric Goldsmith <eric@ericgoldsmith.com>
2013-01-25 14:20:55 -0500

    Added PS2 support for X, Y, Z axis

--- PS2_IK_Control/PS2_IK_Control.ino
+++ PS2_IK_Control/PS2_IK_Control.ino
@@ -15,13 +15,19 @@
 *	and PS2X controller library:
 *		github.com/madsci1016/Arduino-PS2X
 *
+* 	PS2 Controls
+*		Right Joystick L/R:	Gripper tip X position (side to side)
+*		Right Joystick U/D:	Gripper tip Y position (distance out from base center)
+*		R1/R2 buttons:		Gripper tip Z position (height from surface)
+*		Left Joystick U/D:	Wrist angle
+*
 *	Eric Goldsmith
 *	www.ericgoldsmith.com
 *
+* 	Version history
+*		0.1 Initial port of code to use Arduino Server Library
 *
-*  Version history
-*    0.1 Initial port of code to use Arduino Server Library
-*
+*		0.2 Added PS2 controls
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -36,6 +42,7 @@
 ******************************************************************/
 
 #include <Servo.h>
+#include <PS2X_lib.h>
 
 //#define DEBUG				// Uncomment to turn on debugging output
 //#define WRIST_ROTATE		// Uncomment if wrist rotate hardware is installed
@@ -49,7 +56,7 @@
 #define ULNA 327.025        //elbow-to-wrist "bone" 12.875"
 #define GRIPPER 85.725      //gripper length 3.375"
  
-// Arduino pin numbers for server connections
+// Arduino pin numbers for servo connections
 #define BAS_SERVO_PIN 2		// Base servo HS-485HB
 #define SHL_SERVO_PIN 3		// Shoulder Servo HS-805HB
 #define ELB_SERVO_PIN 4		// Elbow Servo HS-755HB
@@ -78,12 +85,28 @@
 #define GRI_MIN 0
 #define GRI_MAX 180
 #endif
+
+// Define pin numbers for PS2 controller connections
+#define PS2_CLK 9
+#define PS2_CMD 7
+#define PS2_ATT 8
+#define PS2_DAT 6
+
+// Joystick characteristics
+#define JS_MIDPOINT 128		// Numeric value for joystick midpoint
+#define JS_DEADBAND 4		// Ignore movement this close to the center position
+#define JS_SCALE_FACTOR 100	// Divisor for scaling JS output
+#define Z_INCREMENT			// Change in Z axis per button press
  
 // Pre-calculations
 float hum_sq = HUMERUS*HUMERUS;
 float uln_sq = ULNA*ULNA;
 
-// Server objects 
+// PS2 Controller object
+PS2X	Ps2x;
+byte	ps2_stat;
+
+// Servo objects 
 Servo	Bas_Servo;
 Servo	Shl_Servo;
 Servo	Elb_Servo;
@@ -93,6 +116,12 @@ Servo	Gri_Servo;
 #ifdef WRIST_ROTATE
 Servo	Wro_Servo;
 #endif
+
+// Global values for arm position
+float X = 0				// mm - left/right from base centerline. 0 = straight
+float Y = 100			// mm - away (out) from base center
+float Z = 100			// mm - up from surface
+float Grip_Angle = 90;	// degrees - wrist angle. 90 = horizontal
  
 void setup()
 {
@@ -100,6 +129,25 @@ void setup()
 	Serial.begin(115200);
 #endif
 
+	// Setup PS2 controller pins and settings and check for error
+	// 	GamePad(clock, command, attention, data, Pressures?, Rumble?)
+	ps2_stat = Ps2x.config_gamepad(PS2_CLK,PS2_CMD,PS2_ATT,PS2_DAT, true, true);
+ 
+#ifdef DEBUG
+	if (ps2_stat == 0)
+		Serial.println("Found Controller, configured successfully.");
+
+	else if (ps2_stat == 1)
+		Serial.println("No controller found.");
+
+	else if (ps2_stat == 2)
+		Serial.println("Controller found but not accepting commands.");
+
+	else if (ps2_stat == 3)
+		Serial.println("Controller refusing to enter 'Pressures' mode, may not support it. ");      
+#endif
+
+	// Attach to the servos
 	Bas_Servo.attach(BAS_SERVO_PIN);
 	Shl_Servo.attach(SHL_SERVO_PIN);
 	Elb_Servo.attach(ELB_SERVO_PIN);
@@ -110,6 +158,7 @@ void setup()
 #endif
 
 	servo_park();
+	
 #ifdef DEBUG
 	Serial.println("Start");
 #endif
@@ -119,9 +168,43 @@ void setup()
 void loop()
 {
 
-  //zero_x();
-  line();
-  //circle();
+	if (ps2_stat == 1) //skip loop if no controller found
+		return; 
+
+	Ps2x.read_gamepad();          //read controller
+
+	// Read the left and right joysticks and translate the 
+	// normal range of values (0-255) to zero-centered values (-128 - 128)
+	byte ly_trans = JS_MIDPOINT - ps2x.Analog(PSS_LY);
+	byte lx_trans = ps2x.Analog(PSS_LX) - JS_MIDPOINT;
+	byte ry_trans = JS_MIDPOINT - ps2x.Analog(PSS_RY);
+	byte rx_trans = ps2x.Analog(PSS_RX) - JS_MIDPOINT;
+
+	// X Position
+	// Can be positive or negative, so no range checks needed
+	// TODO: incorporate current servo position feedback
+	if (abs(lx_trans) > DEADBAND)
+		X += (lx_trans / JS_SCALE_FACTOR);
+
+	// Y Position
+	// Can only be positive, so enforce lower bound
+	// TODO: incorporate current servo position feedback
+	if (abs(ly_trans) > DEADBAND)
+		Y += max((ly_trans / JS_SCALE_FACTOR), 0);
+
+	// Z Position
+	// Can only be positive, so enforce lower bound
+	// TODO: incorporate current servo position feedback
+	if (Ps2x.Button(PSB_R1) || Ps2x.Button(PSB_R2)) {
+		if (Ps2x.Button(PSB_R1))
+			Z += Z_INCREMENT;				// up
+		else
+			Z = max((Z - Z_INCREMENT), 0);	// down
+	}
+	
+	set_arm(X, Y, Z, Grip_Angle);
+
+	delay(50);
  }
  
 // Arm positioning routine utilizing inverse kinematics
@@ -185,10 +268,14 @@ void set_arm(float x, float y, float z, float grip_angle_d)
 // Move servos to parking position
 void servo_park()
 {
-  	Bas_Servo.write(SERVO_MIDPOINT);
+
+	set_arm(X, Y, Z, Grip_Angle);
+/*
+	Bas_Servo.write(SERVO_MIDPOINT);
 	Shl_Servo.write(SERVO_MIDPOINT);
 	Elb_Servo.write(SERVO_MIDPOINT);
 	Wri_Servo.write(SERVO_MIDPOINT);
+*/	
 	Gri_Servo.write(SERVO_MIDPOINT);
 #ifdef WRIST_ROTATE
 	Wro_Servo.write(SERVO_MIDPOINT);
